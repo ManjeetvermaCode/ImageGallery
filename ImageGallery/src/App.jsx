dccbde7bcfeb0b8c0a0515d5433b0e57b96c5273
manjeetverma <manjeetverma@gmail.com>
2023-09-23 13:02:15 +0530

    added navbar to the app

--- ImageGallery/src/App.jsx
+++ ImageGallery/src/App.jsx
@@ -1,11 +1,29 @@
+import { useState } from 'react'
 
-import './App.css'
+import './styles/header-styles.css'
+
+//components
+import NavBar from './components/navBar'
 
 function App() {
+const [search,setSearch]=useState('')
 
+
+const submitHandler=(e)=>{
+  e.preventDefault()
+  console.log(search)
+}
   return (
     <>
-     <h1>Hello</h1>
+    <NavBar/>
+      <div className="header">
+        <h2 className='title'>Image Gallery</h2>
+        <h3 className='search-title'>Search Image</h3>
+        <form onSubmit={submitHandler}>
+          <input className='input-field' type="text" placeholder='for example- Train' onChange={(e)=>setSearch(e.target.value)} />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
     </>
   )
 }
---
+++ ImageGallery/src/components/navBar.jsx
@@ -0,0 +1,12 @@
+
+import '../styles/navbar-styles.css'
+
+export default function NavBar() {
+    return(
+        <div className="container">
+            <p>Home</p>
+            <h3 >Image Gallery Application</h3>
+            <a href="#" disabled >Profile</a>
+        </div>
+    )
+}
